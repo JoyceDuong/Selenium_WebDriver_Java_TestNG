e9840eeca21d8ad3daaa068e27af6190b3188ab3
Admin <Admin@DESKTOP-7A005MA>
2021-10-20 17:39:07 +0700

    update testng Lib

---
+++ selenium/webdriver/NewTest.java
@@ -0,0 +1,20 @@
+package webdriver;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void TC10() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
