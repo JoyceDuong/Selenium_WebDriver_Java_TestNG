1b60179d999dd466a38728e4d19135805ea4b594
Admin <Admin@10.0.91.113>
2021-10-22 11:26:44 +0700

    Locator

--- selenium/webdriver/NewTest.java
+++ selenium/webdriver/NewTest.java
@@ -1,20 +1,42 @@
 package webdriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class NewTest {
-	@Test
-	public void TC10() {
-	}
-
+	WebDriver driver; 
 	@BeforeClass
 	public void beforeClass() {
+		
+	}
+	@Test
+	public void OpenFirefox() {
+		
+		driver = new FirefoxDriver();
+		
+		driver.get("https://demo.nopcommerce.com/");
 	}
 
+	@Test
+	public void byID() throws InterruptedException {
+		driver.findElement(By.className("search-box-text ")).sendKeys("duongminhhieudeptrai");
+		Thread.sleep(2000);
+		
+	}
+	
+	@Test
+	public void byClass() throws InterruptedException {
+		driver.findElement(By.className("button-1 ")).click();
+		Thread.sleep(2000);
+	}
+	
 	@AfterClass
 	public void afterClass() {
+		driver.quit();
 	}
 
 }
