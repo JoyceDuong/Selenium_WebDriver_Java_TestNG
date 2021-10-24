5790cb8edaba86bd814854566be0346171074ed9
admin <admin@DESKTOP-JMK0BG4>
2021-10-24 23:50:10 +0700

    BT

--- selenium/webdriver/Topic04_Xpath_Technical02.java
+++ selenium/webdriver/Topic04_Xpath_Technical02.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -23,13 +24,24 @@ public class Topic04_Xpath_Technical02 {
 	public void beforeClass() {
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
 	}
 
 	@Test
-	public void TC01() {
+	public void TC01_Login_Empty_Filed() {
 		
+		driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']")).click();
+		
+		Assert.assertEquals(driver.findElement(By.id("txtFirstname-error")).getText(),"Vui lòng nhập họ tên");
+		Assert.assertEquals(driver.findElement(By.id("txtEmail-error")).getText(),"Vui lòng nhập email");	
+		Assert.assertEquals(driver.findElement(By.id("txtCEmail-error")).getText(),"Vui lòng nhập lại địa chỉ email");	
+		Assert.assertEquals(driver.findElement(By.id("txtPassword-error")).getText(),"Vui lòng nhập mật khẩu");	
+		Assert.assertEquals(driver.findElement(By.id("txtCPassword-error")).getText(),"Vui lòng nhập lại mật khẩu");	
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(),"Vui lòng nhập số điện");	
+
+
+
 	}
 	
 	
